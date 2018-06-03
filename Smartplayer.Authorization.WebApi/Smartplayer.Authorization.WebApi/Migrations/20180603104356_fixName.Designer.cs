8c66bfdd2142e764d6a2371a26ec282d46840a38
Sebastian Sarnecki <ssarnecki34@gmail.com>
2018-06-03 12:47:37 +0200

    fix

--- Smartplayer.Authorization.WebApi/Smartplayer.Authorization.WebApi/Migrations/20180603104356_fixName.Designer.cs
+++ Smartplayer.Authorization.WebApi/Smartplayer.Authorization.WebApi/Migrations/20180603104356_fixName.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Smartplayer.Authorization.WebApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180603104356_fixName")]
+    partial class fixName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
